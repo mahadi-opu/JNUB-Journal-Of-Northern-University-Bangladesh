dbce463922a2c542b8edb50f45ebde16d891c418
smhasanopu <smhasanopu@gmail.com>
2023-06-25 12:10:16 +0600

    git march size commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-s
+yy
                     </ul>
 
                     <!-- Right Side Of Navbar -->
